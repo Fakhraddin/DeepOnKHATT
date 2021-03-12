a19d4fe14763d256f75e8dd32fc2c51d3da05676
unknown <fakhri100@gmail.com>
2021-03-13 00:33:26 +0200

    commit1

--- text.py
+++ text.py
@@ -10,6 +10,88 @@ SPACE_TOKEN = '<space>'
 SPACE_INDEX = 0
 FIRST_INDEX = ord('a') - 1  # 0 is reserved to space
 
+def decodex(txt,mapping):
+    out=''
+    for ch in txt:
+        out=out+mapping[ch]
+    return out
+def get_arabic_letters(file_name='Arabic_Mappping.csv'):
+  letters_ar= {}
+  with open(file_name,encoding="utf-8") as f:
+    for line in f:
+       (key, val) = line.split(',')
+       letters_ar[int(key)] =val.strip() 
+  letters_ar[83]=' '
+  return letters_ar
+
+class Alphabet(object):
+    def __init__(self, config_file):
+        self._config_file = config_file
+        self._label_to_str = {}
+        self._str_to_label = {}
+        self._size = 0
+        if config_file:
+            with codecs.open(config_file, 'r', 'utf-8') as fin:
+                for line in fin:
+                    if line[0:2] == '\\#':
+                        line = '#\n'
+                    elif line[0] == '#':
+                        continue
+                    self._label_to_str[self._size] = line[:-1] # remove the line ending
+                    self._str_to_label[line[:-1]] = self._size
+                    self._size += 1
+
+    def _string_from_label(self, label):
+        return self._label_to_str[label]
+
+    def _label_from_string(self, string):
+        try:
+            return self._str_to_label[string]
+        except KeyError as e:
+            raise KeyError(
+                'ERROR: Your transcripts contain characters (e.g. \'{}\') which do not occur in data/alphabet.txt! Use ' \
+                'util/check_characters.py to see what characters are in your [train,dev,test].csv transcripts, and ' \
+                'then add all these to data/alphabet.txt.'.format(string)
+            ).with_traceback(e.__traceback__)
+
+    def has_char(self, char):
+        return char in self._str_to_label
+
+    def encode(self, string):
+        res = []
+        for char in string:
+            res.append(self._label_from_string(char))
+        return res
+
+    def decode(self, labels):
+        res = ''
+        for label in labels:
+            res += self._string_from_label(label)
+        return res
+
+    def serialize(self):
+        # Serialization format is a sequence of (key, value) pairs, where key is
+        # a uint16_t and value is a uint16_t length followed by `length` UTF-8
+        # encoded bytes with the label.
+        res = bytearray()
+
+        # We start by writing the number of pairs in the buffer as uint16_t.
+        res += struct.pack('<H', self._size)
+        for key, value in self._label_to_str.items():
+            value = value.encode('utf-8')
+            # struct.pack only takes fixed length strings/buffers, so we have to
+            # construct the correct format string with the length of the encoded
+            # label.
+            res += struct.pack('<HH{}s'.format(len(value)), key, len(value), value)
+        return bytes(res)
+
+    def size(self):
+        return self._size
+
+    def config_file(self):
+        return self._config_file
+
+
 
 def normalize_txt_file(txt_file, remove_apostrophe=True):
     """
